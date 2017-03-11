3577285a2b6f6246203c134997b14ef667b189f1
Jean-Simon <jeansimoncollard@hotmail.com>
2017-03-10 22:30:50 -0500

    added distance calculater

--- Dataasp/Default.aspx.cs
+++ Dataasp/Default.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dataasp.Backend.DistanceCalculter;
+using Dataasp.Backend.GoogleMaps.DistanceCalculter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +11,23 @@ namespace Dataasp
 {
     public partial class _Default : Page
     {
+        private AddressLatLongConverter _addressLatLongConverter;
+        private DistanceCalculater _distanceCalculater;
         protected void Page_Load(object sender, EventArgs e)
         {
+            _addressLatLongConverter = new AddressLatLongConverter();
+            _distanceCalculater = new DistanceCalculater();
+        }
+
+        protected void addTripButton_Click(object sender, EventArgs e)
+        {
+            var startAddress = fromTextBox.Text;
+            var endAddress = toTextBox.Text;
+            var startCoordinates = _addressLatLongConverter.GetLatLong(startAddress);
+            var endCoordinates = _addressLatLongConverter.GetLatLong(endAddress);
+
+            var distance = _distanceCalculater.GetDistance(startAddress, endAddress, travelModeComboBox.SelectedValue);
+
 
         }
     }
