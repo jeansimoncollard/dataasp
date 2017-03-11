a6f8546063b757dd3961c3cd0c91e207752f1775
Jean-Simon <jeansimoncollard@hotmail.com>
2017-03-11 10:02:06 -0500

    added gmap.net api

---
+++ Dataasp/Backend/GoogleMaps/WayPointGeneration/WaypointValidater.cs
@@ -0,0 +1,19 @@
+﻿using GoogleMaps.LocationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dataasp.Backend.GoogleMaps.WayPointGeneration
+{
+    public class WaypointValidater
+    {
+        public bool IsWaypointViable(string startAddress, string endAddress, MapPoint wayPoint)
+        {
+            var startCoordinates = _addressLatLongConverter.GetLatLong(startAddress);
+            var endCoordinates = _addressLatLongConverter.GetLatLong(endAddress);
+
+            return false;
+        }
+    }
+}
