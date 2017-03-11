f8688ffac319b12706f93bebdceed1f6a2798e50
Jean-Simon <jeansimoncollard@hotmail.com>
2017-03-11 15:28:36 -0500

    fixed machinekey error and progress on marker

---
+++ Dataasp/Backend/GoogleMaps/MapGeneration/MarkerGenerater.cs
@@ -0,0 +1,34 @@
+﻿using Dataasp.Backend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace Dataasp.Backend.GoogleMaps.MapGeneration
+{
+    public class MarkerGenerater
+    {
+        public string GenerateMarkers(List<MarkerEntity> markers)
+        {
+            var futureString = new List<char>();
+            foreach (var marker in markers)
+            {
+                futureString.AddRange(generateMaker(marker.Latitude, marker.Longitude, marker.MarkerTitle));
+            }
+            return "";
+        }
+        private List<char> generateMaker(double latitude, double longitude, string markerTitle)
+        {
+            return new List<char>($@"
+            <script>
+                var marker = new google.maps.Marker({{
+                position: {{lat: {latitude.ToString(CultureInfo.CreateSpecificCulture("en-US"))}, lng: {longitude.ToString(CultureInfo.CreateSpecificCulture("en-US"))}}},
+                map: map,
+                title: '{markerTitle}'
+              }});
+            </script>
+        ");
+        }
+    }
+}
