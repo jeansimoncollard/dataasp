95a7ffa7b3ace671a92fd8a30cf6d64953283be5
Jean-Simon <jeansimoncollard@hotmail.com>
2017-03-10 20:54:17 -0500

    repo setup

---
+++ Dataasp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Dataasp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Dataasp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dataasp.Global" Language="C#" %>
