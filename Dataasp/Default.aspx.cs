65076d0e72b370fbd0bb630b94de27215f8c998a
Alexandre Charbonneau <acharbonneau16@ubishops.ca>
2017-03-11 00:46:21 -0500

    merged

--- Dataasp/Default.aspx.cs
+++ Dataasp/Default.aspx.cs
@@ -1,31 +1,41 @@
-﻿using Dataasp.Backend.DistanceCalculter;
-using Dataasp.Backend.Enums;
+
+using Dataasp.Backend.Quickstats;
+using Dataasp.Backend.DistanceCalculter;
 using Dataasp.Backend.GoogleMaps.DistanceCalculter;
-using Dataasp.Backend.GoogleMaps.MapGeneration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dataasp.Backend.GoogleMaps.MapGeneration;
+using Dataasp.Backend.Enums;
+using Dataasp.Backend.jstemporaryclickbutton;
 
 namespace Dataasp
 {
     public partial class _Default : Page
     {
+        Quickstats _quickstats = new Quickstats();
+
+        private AddressLatLongConverter _addressLatLongConverter;
         private DistanceCalculater _distanceCalculater;
         private MapGeneraterAdapter _mapGeneraterAdapter;
         private StringToTravelEnumConverter _stringToTravelEnumConvert;
+        private jstemporarybuttonclicker _jstemporarybuttonclicker;
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            _addressLatLongConverter = new AddressLatLongConverter();
             _distanceCalculater = new DistanceCalculater();
             _mapGeneraterAdapter = new MapGeneraterAdapter();
             _stringToTravelEnumConvert = new StringToTravelEnumConverter();
+            _jstemporarybuttonclicker = new jstemporarybuttonclicker();
         }
 
         protected void addTripButton_Click(object sender, EventArgs e)
         {
+            var div = quickStatsDiv;
             var startAddress = fromTextBox.Text;
             var endAddress = toTextBox.Text;
             if (toTextBox.Text.Length == 0 || fromTextBox.Text.Length == 0)
@@ -36,12 +46,13 @@ namespace Dataasp
             }
             var startCoordinates = _addressLatLongConverter.GetLatLong(startAddress);
             var endCoordinates = _addressLatLongConverter.GetLatLong(endAddress);
-
+            _jstemporarybuttonclicker.clicked();
             mapResults.InnerHtml = _mapGeneraterAdapter.GenerateMap(startAddress, endAddress, _stringToTravelEnumConvert.Convert(travelModeComboBox.SelectedValue));
 
             var distance = _distanceCalculater.GetDistance(startAddress, endAddress, travelModeComboBox.SelectedValue);
 
             div.Attributes.Remove("class"); //removes the danger class highlight
+
             _quickstats.SetDistance(distance);
             _quickstats.SetName("Alex");
             _quickstats.SetMeansOfTransportation(travelModeComboBox.SelectedValue);
