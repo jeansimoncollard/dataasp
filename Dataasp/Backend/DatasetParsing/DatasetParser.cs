8f07734d5fb365a069995080aee4efac18f884b2
Jean-Simon <jeansimoncollard@hotmail.com>
2017-03-12 01:50:03 -0500

    markes for landscape added

--- Dataasp/Backend/DatasetParsing/DatasetParser.cs
+++ Dataasp/Backend/DatasetParsing/DatasetParser.cs
@@ -9,18 +9,18 @@ namespace Dataasp.Backend.DatasetParsing
     {
         private ConstructionSitesParser _constructionSitesParser;
         private SpeedTrapParser _speedTrapParser;
-        private CarAccidentParser _carAccidentParser;
+        private LandscapeParser _landscapeParser;
         public DatasetParser()
         {
             _constructionSitesParser = new ConstructionSitesParser();
             _speedTrapParser = new SpeedTrapParser();
-            _carAccidentParser = new CarAccidentParser();
+            _landscapeParser = new LandscapeParser();
         }
         public void ParseDatasets()
         {
             _constructionSitesParser.ParseConstructionSites(@"http://www.quebec511.info/donneespubliques/entraves.ashx?format=csv");
             _speedTrapParser.Parse(@"https://www.donneesquebec.ca/recherche/dataset/8f8766ba-47da-4043-804d-629c445da97d/resource/63b7150e-062c-48ed-a77b-35fe552a11a4/download/radarsphoto.csv");
-            _carAccidentParser.Parse(@"https://saaq.gouv.qc.ca/donnees-ouvertes/rapports-accident/rapports-accident-2015.csv");
+            _landscapeParser.Parse(@"http://ws.mapserver.transports.gouv.qc.ca/donnees/geomatique/lieuhabite.csv");
 
         }
 
